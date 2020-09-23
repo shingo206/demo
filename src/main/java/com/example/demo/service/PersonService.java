4545e7f26c1eae5f3eeb0dffc2a35e4bf8ada28f
Shingo Tamura <Shingo.Tamura@neeur.com>
2020-09-23 14:52:29 +0200

    Complete deployment with jar file. Add lombok.config to work with @Qualifier annotation.

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -3,15 +3,20 @@ package com.example.demo.service;
 import com.example.demo.dao.PersonDao;
 import com.example.demo.model.Person;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor(onConstructor = @__({@Autowired}))
 public class PersonService {
+    @NonNull
+    @Qualifier("mySql")
     private final PersonDao personDao;
 
     public int addPerson(Person person) {
